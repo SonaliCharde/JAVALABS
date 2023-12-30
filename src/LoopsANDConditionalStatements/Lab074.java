7ce2b89fb0bbf249b6fdf357585e9a3224d5caf2
SonaliCharde <chardesonaligithub@gmail.com>
2023-12-30 13:14:01 +0530

    Java Prgrams written in Intellij

---
+++ src/LoopsANDConditionalStatements/Lab074.java
@@ -0,0 +1,10 @@
+package LoopsANDConditionalStatements;
+
+public class Lab074 {
+    public static void main(String[]args){
+        final boolean b1 = true;
+         for (float i = 0; b1;){
+             System.out.println("TTA"); // Infinite loop as the condition is always true
+         }
+    }
+}
---
+++ src/LoopsANDConditionalStatements/Lab075.java
@@ -0,0 +1,18 @@
+package LoopsANDConditionalStatements;
+
+public class Lab075 {
+    public static void main(String[]args){
+       /* for (;;){
+            System.out.println("Sonali"); // Infinite loop with exit code 130 when stopped forcefully
+        }*/
+       /* boolean b1 = true;
+        b1=false;
+        for (;b1;){
+            System.out.println("Sonali"); // Exit code 0 No output
+        }*/
+
+        for (double i =1.1; i< 12.30; i++){
+            System.out.println(i);
+        }
+    }
+}
